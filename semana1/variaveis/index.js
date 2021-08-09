a7dfda38bd89e303959612ad58be2e7e63844794
Jacqueline Canavarro <canavarrojacqueline@gmail.com>
2021-08-09 18:19:47 -0300

    Subindo tarefa de variaveis referente a semana 1 do curso

---
+++ semana1/variaveis/index.js
@@ -0,0 +1,97 @@
+/* RESPOSTA 1
+
+let a = 10
+let b = 10
+
+console.log(b) RESPOSTA: 10
+
+b=5
+
+console.log(a, b) RESPOSTA: 10, 5
+
+RESPOSTA 2
+
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log (a, b, c) RESPOSTA: 10, 20, 10
+
+RESPOSTA 3
+
+let p = prompt("Quantas horas você trabalha por dia?") RESPOSTA: let hrsTrabalhoDiarios
+let t = prompt ("Quanto você recebe por dia?") RESPOSTA: let qtRecebeDiariamente*/
+
+
+/* let nome 
+let idade 
+
+console.log (typeof nome)
+console.log (typeof idade)/*
+
+
+/* O comentário foi impressso porque coloquei o let, que permite alterações
+ao longo do projeto e não coloquei o sinal de "="*/
+
+/* let nome = "Jacqueline"
+let idade = 32
+
+/* console.log ("Meu nome é", nome, "e eu tenho", idade, "anos") */
+
+/* console.log (typeof nome)
+console.log (typeof idade) */
+
+/* notei que agora que coloquei entre aspas o nome, o sistema reconheceu
+que é uma spring, e como não coloquei aspas na idade, ele entendeu que era número */
+
+// let nome = "Jacqueline"
+// let idade = 32
+
+// console.log ("Olá,", nome,",","você tem", idade, "anos")
+
+// let gripe = "Você está gripada hoje?"
+// let cor = "Você está usando preto hoje?"
+// let agua = "Você tomou água hoje?"
+
+// let gripeResposta = "nao"
+// let corResposta = "nao"
+// let aguaResposta = "sim"
+
+// console.log (gripe,"-",gripeResposta)
+// console.log (cor,"-",corResposta)
+// console.log (agua,"-",aguaResposta)
+
+//let a = 50
+//let b = 70
+
+//let a1= 70
+//let b1 = 50
+
+//let aVaiSera1dps = true
+//let bVaiSerb1dps = true
+
+//console.log ("O novo valor de A é", a1)
+//console.log ("O novo valor de B é", b1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
